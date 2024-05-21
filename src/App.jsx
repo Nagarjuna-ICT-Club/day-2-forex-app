e78c0f41d17a91caa4653f08bd34436697240104
rikesh-a11 <rikeshshrestha499@gmail.com>
2024-05-21 09:35:08 +0545

    day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-
+  const [data, setData] = React.useState([]);
+  const APIUrl =
+    "https://www.nrb.org.np/api/forex/v1/rates?page=1&from=2024-05-20&to=2024-05-20&per_page=100";
+  const callApi = () => {
+    axios.get(APIUrl).then((response) => {
+      const responseFromNRB = response.data;
+      setData(responseFromNRB.data.payload[0]);
+    });
+  };
+  React.useEffect(() => {
+    callApi();
+  }, []);
   return (
     <>
-      <div>
-        <h2>Day 2 | Forex-App</h2>
-        </div>
+      <div className="ml-10 mt-5">
+        <p>Date: {data?.date}</p>
+        {data.rates?.map((rate, key) => {
+          return (
+            <div key={key} className="flex gap-2">
+              <p>{rate.currency.name}</p>
+              <p>{rate.buy}</p>
+              <p>{rate.sell}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
